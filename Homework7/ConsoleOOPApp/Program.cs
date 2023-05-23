ecd781c23563345e8b738140eb1f894eee250495
adeqidroze <adeqidroze@gmail.com>
2023-05-23 05:53:20 +0400

    Homework 9 part1

---
+++ Homework7/ConsoleOOPApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleOOPApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var week = 7;
+            Company commSchool = new Company();
+            commSchool.isLocal = true;
+
+            double totalAmount = Company.TaxToPay(commSchool.isLocal, inputs.singleInputValidator());
+            Console.WriteLine($"The total income of your company is {totalAmount}");
+
+            Employee zura = new Employee();
+            Console.WriteLine("Please enter employee name:");
+            zura.name = Console.ReadLine();
+            Console.WriteLine("Please enter employee lastname:");
+            zura.lastName = Console.ReadLine();
+            Console.WriteLine("We need to know the age too!");
+            zura.age = inputs.singleInputValidator();
+            zura.employeePos = Employee.Positions.QA;
+            Console.WriteLine("Log employee's work hours during this week.");
+            zura.workedHoursInWeek = inputs.inputArrayValidator(week);
+
+            double employeeIncome = Employee.WeeklyIncome(zura.employeePos, zura.workedHoursInWeek);
+            Console.WriteLine($"Your Weekly Income Is {employeeIncome}.");
+
+
+        }
+    }
+}
